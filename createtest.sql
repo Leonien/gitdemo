5b155d3b69597d6d2b842363a382f8206ecfbab2
John Cross <Leonien@gmail.com>
2019-02-07 09:53:43 +0000

    Changes 4

--- createtest.sql
+++ createtest.sql
@@ -22,4 +22,4 @@ VALUES(12,'TWO TOWERS','SECOND IN TRILOGY','TOLKIEN','PENGIUN','01-JAN-78',1);
 INSERT INTO loans (student_id,isbn,loan_date)
 VALUES(2000,12,'20-JAN-78');
 
-CREATE TABLE pancakes {};
+CREATE TABLE cheeses {};
